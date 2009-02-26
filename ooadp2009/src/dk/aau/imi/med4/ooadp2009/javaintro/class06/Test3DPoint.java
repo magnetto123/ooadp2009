f1c3859c12b070887a5051b02167e67cd4000968
chromamorph <chromamorph@5ac1dc98-fcf6-11dd-ac65-f74f249cfaac>
2009-02-26 22:07:51 +0000

    Explanation of the toString method

--- ooadp2009/src/dk/aau/imi/med4/ooadp2009/javaintro/class06/Test3DPoint.java
+++ ooadp2009/src/dk/aau/imi/med4/ooadp2009/javaintro/class06/Test3DPoint.java
@@ -5,13 +5,13 @@ package dk.aau.imi.med4.ooadp2009.javaintro.class06;
  */
 public class Test3DPoint {
 	public static void main(String[] args) {
-		
+
 		// Let's create a ThreeDimensionalPoint using the no-args constructor.
 		ThreeDimensionalPoint p = new ThreeDimensionalPoint();
 		System.out.println("p.x = " + p.getX() // Inherited from Point
 				+ ", p.y = " + p.getY() // Inherited from Point
 				+ ", p.z = " + p.getZ() // Defined in ThreeDimensionalPoint
-				);
+		);
 
 		p.setX(2.0); // Inherited from Point
 		p.setY(3.0); // Inherited from Point
@@ -19,6 +19,21 @@ public class Test3DPoint {
 		System.out.println("p.x = " + p.getX() // Inherited from Point
 				+ ", p.y = " + p.getY() // Inherited from Point
 				+ ", p.z = " + p.getZ() // Defined in ThreeDimensionalPoint
-				);
+		);
+
+		// Now we create a ThreeDimensionalPoint using the constructor that sets
+		// the values of the x, y and x fields.
+		p = new ThreeDimensionalPoint(5.0, 6.0, 7.0);
+		System.out.println("p.x = " + p.getX() // Inherited from Point
+				+ ", p.y = " + p.getY() // Inherited from Point
+				+ ", p.z = " + p.getZ() // Defined in ThreeDimensionalPoint
+		);
+
+		// Demonstrating the toString methods on Point and ThreeDimensionalPoint
+		Point q = new Point(3,4);
+		System.out.println("q is " + q); // Automatically calls q.toString()
+		System.out.println("p is " + p); // Automatically calls p.toString()
+		
+		
 	}
 }
