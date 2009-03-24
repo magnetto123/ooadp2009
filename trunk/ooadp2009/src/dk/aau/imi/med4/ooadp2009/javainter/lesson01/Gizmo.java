9bc8016c0c7ca1667bcd6f957b5a4f017a11e90e
chromamorph <chromamorph@5ac1dc98-fcf6-11dd-ac65-f74f249cfaac>
2009-03-24 14:42:11 +0000

    Gizmo class in lesson 1

---
+++ trunk/ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson01/Gizmo.java
@@ -0,0 +1,60 @@
+package dk.aau.imi.med4.ooadp2009.javainter.lesson01;
+
+/**
+ * Explains use of a static field to store number of a particular type of object
+ * that has been created.
+ */
+public class Gizmo {
+
+	/**
+	 * numberOfGizmos stores the number of Gizmos that have been constructed. It
+	 * is static, which means that there is only one copy of this variable,
+	 * stored in the class itself. Each Gizmo object does not have its own copy
+	 * of this field.
+	 */
+	static int numberOfGizmos = 0;
+
+	/**
+	 * canIAlsoBeTheNumberOfGizmos cannot also be the number of Gizmos because
+	 * it isn't static.
+	 */
+	int canIAlsoBeTheNumberOfGizmos = 0;
+
+	/**
+	 * id stores the id of this Gizmo
+	 */
+	int id = 0;
+
+	/**
+	 * canIAlsoBeTheId can't be the ID because it is set using
+	 * canIAlsoBeTheNumberOfGizmos which isn't static.
+	 */
+	int canIAlsoBeTheId = 0;
+
+	Gizmo() {
+		// Increase static field numberOfGizmos before setting id.
+		id = ++numberOfGizmos;
+
+		// Following will always be set to 1.
+		canIAlsoBeTheId = ++canIAlsoBeTheNumberOfGizmos;
+	}
+
+	public String toString() {
+		return "Gizmo: id=" + id + "; canIAlsoBeTheId=" + canIAlsoBeTheId
+				+ "; canIAlsoBeTheNumberOfGizmos="
+				+ canIAlsoBeTheNumberOfGizmos;
+	}
+
+	public static void main(String[] args) {
+		Gizmo[] gizmoArray = new Gizmo[10];
+		for (int i = 0; i < gizmoArray.length; i++)
+			gizmoArray[i] = new Gizmo();
+		for (Gizmo gizmo : gizmoArray)
+			System.out.println(gizmo);
+
+		System.out.println("numberOfGizmos = " + Gizmo.numberOfGizmos);
+		// System.out.println("canIAlsoBeTheNumberOfGizmos = " +
+		// Gizmo.canIAlsoBeTheNumberOfGizmos);
+	}
+
+}
