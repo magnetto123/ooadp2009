f524013bf6bab6492a746f57e75384fccf6abf52
chromamorph <chromamorph@5ac1dc98-fcf6-11dd-ac65-f74f249cfaac>
2009-03-24 22:11:02 +0000

    Created StaticFields main method class and FinalFields main method class

---
+++ trunk/ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson02/FinalFields.java
@@ -0,0 +1,21 @@
+package dk.aau.imi.med4.ooadp2009.javainter.lesson02;
+
+public class FinalFields {
+	public static void main(String[] args) {
+
+		// Let's print out the value of the final static field, pi.
+		System.out.println("Fooble.pi = " + Fooble.pi);
+
+		// Now try to change value of pi field in Fooble
+		// Fooble.pi = 22.0/7;
+		// Can't do it because the pi field is final and it has already been
+		// assigned.
+
+		// Now we create a Fooble object. It's final ip field is
+		// initialized to 1.413 in the declaration of the ip field.
+		Fooble f = new Fooble();
+		System.out.println("f.ip = " + f.ip);
+		System.out.println("f.beep = " + f.beep);
+	}
+
+}
