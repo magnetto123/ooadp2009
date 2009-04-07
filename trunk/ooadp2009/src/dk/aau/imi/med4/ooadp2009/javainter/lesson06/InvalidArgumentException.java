d0aa5413ee65099cb1c1739a20d52e1180affd8a
chromamorph <chromamorph@5ac1dc98-fcf6-11dd-ac65-f74f249cfaac>
2009-04-07 19:34:31 +0000

    javainter.lesson06

---
+++ trunk/ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson06/InvalidArgumentException.java
@@ -0,0 +1,15 @@
+package dk.aau.imi.med4.ooadp2009.javainter.lesson06;
+
+public class InvalidArgumentException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidArgumentException() {
+		super();
+	}
+	
+	public InvalidArgumentException(String s) {
+		super(s);
+	}
+
+}
