6b9ec0acb6722f2e6b596f6bdb30b0c94ce8bdcc
chromamorph <chromamorph@5ac1dc98-fcf6-11dd-ac65-f74f249cfaac>
2010-02-13 22:28:47 +0000

    JavaIntro01HelloWorldOrArg added

--- trunk/ooadp2009/src/dk/aau/imi/med4/ooadp2009/javaintro/JavaIntro01HelloWorldOrArg.java
+++ trunk/ooadp2009/src/dk/aau/imi/med4/ooadp2009/javaintro/JavaIntro01HelloWorldOrArg.java
@@ -3,14 +3,17 @@ package dk.aau.imi.med4.ooadp2009.javaintro;
 /**
  * A program that prints the string "Hello world!" to the console.
  */
-public class JavaIntro01HelloWorld {
+public class JavaIntro01HelloWorldOrArg {
 	
 	/**
 	 * Main method
 	 * @param args Array of Strings containing command-line parameters.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		if (args.length==0)
+			System.out.println("Hello world!");
+		else
+			System.out.println("Hello "+args[0]);
 	}
 }
 
